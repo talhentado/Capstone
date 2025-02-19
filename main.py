fe9c9acea26f22b4997c98fd98739d5d6ef02ead
talhentado <bcbelen01@gmail.com>
2025-02-19 16:16:46 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("CAPSTONE PROJECT")
+st.subheader("Certified Python Analytics Associate")
+st.write("BBReyes, BAKAWAN Data Analytics, Inc.")
+st.image("plumeria.jpg", "Bloom where you are planted.")
